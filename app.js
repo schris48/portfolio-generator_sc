0f31fb884a9f6d7b487ed56b062d80e033b5c402
Schris <tovechris48@gmail.com>
2022-04-30 14:10:45 -0400

    generate-webpage-and-modularize

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('===============');
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
