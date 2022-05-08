7774c513ff1554a6e729f09b1a9de61786ba344f
Schris <tovechris48@gmail.com>
2022-05-08 01:20:54 -0400

    update-html-structure

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -118,23 +120,16 @@ const promptProject = portfolioData => {
         }
     });
 };
-  
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
-// const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
+    const pageHTML = generatePage(portfolioData);
 
-// const pageHTML = generatePage(name, github);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
 
-// const [name, github] = profileDataArgs;
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });  
+});
