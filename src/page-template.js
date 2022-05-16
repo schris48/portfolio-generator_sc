8974f30494da152000d84907b370d76ee45b4167
Schris <tovechris48@gmail.com>
2022-05-16 17:45:34 -0400

    style-and-code-cleanup-final

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,4 @@
 // create the about section
-
 const generateAbout = aboutText => {
   if (!aboutText) {
     return '';
@@ -8,12 +7,13 @@ const generateAbout = aboutText => {
 
   return `
     <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block"><About Me</h2>
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
       <p>${aboutText}</p>
     </section>
     `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
@@ -39,22 +39,24 @@ const generateProjects = projectsArr => {
       ${projectsArr
         .filter(({ feature }) => !feature)
         .map(({ name, description, languages, link}) => {
-        return `
-        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-2 flex-column">
-          <h3 class="portfolio-item-title text-light">${name}</h3>
-          <h5 class="portfolio-languages">
-            Built With:
-            ${languages.join(', ')}
-          </h5>
-          <p>${description}</p>
-          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on Github</a>
-        </div>
-      `;
-      })
-      .join('')}
-    </div>
-  </section>
-  `;
+          console.log(languages);
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-2 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on Github</a>
+          </div>
+        `;
+        })
+        .join('')}
+
+      </div>
+      </section>
+    `;
   };
 
 // Export function to generate entire page
@@ -98,66 +100,3 @@ module.exports = templateData => {
   </html>
   `;
 };
-
-module.exports = { writeFile, copyFile };
-
-
-//   // get array of just featured projects
-//   const featuredProjects = projectsArr.filter(project => {
-//     if (project.feature) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-
-//   // get array of all non-featured projects
-//   const nonFeaturedProjects = projectsArr.filter(project => {
-//     if (!project.feature) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-
-//   const featuredProjectHtmlArr = projectsArr.map(({ name, description, languages, link }) =>  {
-//     return `
-//       <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//         <h3 class="portfolio-item-title text-light">${name}</h3>
-//         <h5 class="portfolio-languages">
-//           Built With:
-//           ${languages.join(', ')}
-//         </h5>
-//         <p>${description}</p>
-//         <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2></i>View Project on Github</a>
-//       </div>
-//     `;
-//     }
-//   );
-
-//   const nonFeaturedProjectHtmlArr = projectsArr.map(
-//     ({ name, description, languages, link }) =>  {
-//       return `
-//         <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//           <h3 class="portfolio-item-title text-light">${name}</h3>
-//           <h5 class="portfolio-languages">
-//             Built With:
-//             ${languages.join(', ')}
-//           </h5>
-//           <p>${description}</p>
-//           <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2></i>View Project on Github</a>
-//         </div>
-//       `;
-//     }
-//   );
-
-//   return `
-//     <section class="my-3" id="portfolio">
-//       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-//       <div class="flex-row justify-space-between">
-//         ${featuredProjectHtmlArr.join('')}
-//         ${nonFeaturedProjectHtmlArr.join('')}
-//       </div>
-//     </section>
-//   `;
-// };
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -21,12 +21,12 @@ const writeFile = fileContent => {
 
 const copyFile = fileContent => {
   return new Promise((resolve, reject) => {
-    fs.copyFile('./dist/index.html', fileContent, err => {
+    fs.copyFile('./dist/index.html', './dist/style.css', err => {
       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
       if (err) {
         reject(err);
         // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function
-        return fs.copyFile();
+        return;
       }
       
       // if everything went well, resolve the Promise and send the successful data to the `.then()` method
